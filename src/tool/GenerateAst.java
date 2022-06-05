f5fd4c277a358906971bc6c2783415c962abe7eb
kornienko199004 <30842914+kornienko199004@users.noreply.github.com>
2022-06-05 13:58:17 +0300

    Control flow (#2)
    * [Control Flow]: conditional execution
    
    * [Control Flow]: add logical operators
    
    * [Control Flow]: add white loop
    
    * [Control Flow]: add for loop
    
    Co-authored-by: aleksey.kornienko <aleksey.kornienko@simbirsoft.com>

--- src/tool/GenerateAst.java
+++ src/tool/GenerateAst.java
@@ -18,6 +18,7 @@ public class GenerateAst {
                 "Binary   : Expr left, Token operator, Expr right",
                 "Grouping : Expr expression",
                 "Literal  : Object value",
+                "Logical  : Expr left, Token operator, Expr right",
                 "Unary    : Token operator, Expr right",
                 "Variable : Token name"
         ));
@@ -26,7 +27,10 @@ public class GenerateAst {
                 "Block      : List<Stmt> statements",
                 "Expression : Expr expression",
                 "Print      : Expr expression",
-                "Var        : Token name, Expr initializer"
+                "If         : Expr condition, Stmt thenBranch," +
+                        " Stmt elseBranch",
+                "Var        : Token name, Expr initializer",
+                "While      : Expr condition, Stmt body"
         ));
     }
 
