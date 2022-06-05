d525f1de5d119919d2b2113805039e4ab239394f
aleksey.kornienko <aleksey.kornienko@simbirsoft.com>
2022-06-05 13:26:41 +0300

    [Control Flow]: add white loop

--- src/tool/GenerateAst.java
+++ src/tool/GenerateAst.java
@@ -29,7 +29,8 @@ public class GenerateAst {
                 "Print      : Expr expression",
                 "If         : Expr condition, Stmt thenBranch," +
                         " Stmt elseBranch",
-                "Var        : Token name, Expr initializer"
+                "Var        : Token name, Expr initializer",
+                "While      : Expr condition, Stmt body"
         ));
     }
 
